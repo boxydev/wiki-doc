4e44ba261c3ac8141dcd518df59e334331087318
Matthieu Mota <matthieumota@gmail.com>
2017-05-26 15:05:40 +0200

    Templating system

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,17 @@ var gulp = require('gulp');
 var markdown = require('gulp-markdown');
 var browserSync = require('browser-sync').create();
 var buildBranch = require('buildbranch');
+var nunjucksRender = require('gulp-nunjucks-render');
 
 gulp.task('markdown', function() {
     return gulp.src('src/**/*.{md,html}')
-        .pipe(markdown())
+        //.pipe(markdown())
+        .pipe(nunjucksRender({
+            path: 'templates',
+            data: {
+                name: 'Matthieu'
+            }
+        }))
         .pipe(gulp.dest('dist'))
         .pipe(browserSync.stream());
 });
