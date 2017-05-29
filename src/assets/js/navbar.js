579ce1ef176e27d3ede9e9f1f317d06edf3784c0
Matthieu Mota <matthieumota@gmail.com>
2017-05-29 14:23:21 +0200

    Temp fix url

--- src/assets/js/navbar.js
+++ src/assets/js/navbar.js
@@ -6,8 +6,8 @@ class Navbar extends React.Component {
             <nav>
                 <div className="wrap">
                     <ul className="nav-list">
-                        <li><a className={this.props.url === '/index.html' ? 'active' : ''} href="/index.html">Accueil</a></li>
-                        <li><a className={this.props.url === '/about.html' ? 'active' : ''} href="/about.html">A propos</a></li>
+                        <li><a className={this.props.url === '/index.html' ? 'active' : ''} href="./index.html">Accueil</a></li>
+                        <li><a className={this.props.url === '/about.html' ? 'active' : ''} href="./about.html">A propos</a></li>
                     </ul>
                 </div>
             </nav>
